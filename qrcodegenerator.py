e67a75ce7ba5766df9e683243849997daf0020fa
Eder Andrade <eder>
2023-08-09 21:39:35 -0300

    DEV: Modify the QR code generation library to allow the alteration of the color of the generated SVG DEV: Adjust the HTML of the flip card to ensure compatibility with Safari on iOS

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -1,10 +1,11 @@
 import os
-import qrcode
-import qrcode.image.svg
-
-img = qrcode.make('https://www.linkedin.com/in/edertech/', image_factory=qrcode.image.svg.SvgImage)
+import segno
 
 #this should be separ
 qrCodePath = './static/images/qrcode.svg'
-if not os.path.exists(qrCodePath):
-    img.save(qrCodePath)
+#if not os.path.exists(qrCodePath):
+#    img.save(qrCodePath)
+
+
+qrcode = segno.make('https://www.linkedin.com/in/edertech/')
+qrcode.save('./static/images/qrcode.svg', dark='#32598f')
